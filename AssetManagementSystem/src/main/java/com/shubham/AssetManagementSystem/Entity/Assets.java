5b23269acd12686e445686688b5d6698ba5cace1
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2025-05-28 08:57:33 +0530

    [ADDED]: This code for Assets Management System Welcome Message

---
+++ AssetManagementSystem/src/main/java/com/shubham/AssetManagementSystem/Entity/Assets.java
@@ -0,0 +1,53 @@
+package com.shubham.AssetManagementSystem.Entity;
+
+import java.util.Date;
+
+public class Assets {
+	
+	private long assetId;
+	private String assetName;
+	private String assetType;
+	private String serialNo;
+	private Date purchaseDate;
+	
+	
+	public long getAssetId() {
+		return assetId;
+	}
+	public void setAssetId(long assetId) {
+		this.assetId = assetId;
+	}
+	public String getAssetName() {
+		return assetName;
+	}
+	public void setAssetName(String assetName) {
+		this.assetName = assetName;
+	}
+	public String getAssetType() {
+		return assetType;
+	}
+	public void setAssetType(String assetType) {
+		this.assetType = assetType;
+	}
+	public String getSerialNo() {
+		return serialNo;
+	}
+	public void setSerialNo(String serialNo) {
+		this.serialNo = serialNo;
+	}
+	public Date getPurchaseDate() {
+		return purchaseDate;
+	}
+	public void setPurchaseDate(Date purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Assets [assetId=" + assetId + ", assetName=" + assetName + ", assetType=" + assetType + ", serialNo="
+				+ serialNo + ", purchaseDate=" + purchaseDate + "]";
+	}
+	
+
+}
